8251185eaa761868c48f2387581e9b15d692b7c8
zanenkn <zane.neikena@gmail.com>
2024-09-25 07:15:02 +0200

    more functions in access codes controller

--- controllers/accessCodes.js
+++ controllers/accessCodes.js
@@ -10,7 +10,7 @@ async function createAccessCode({
 }) {
   try {
     const newCode = await seam.accessCodes.create({
-      device_id: '5a534c36-9fa0-4a5d-8d7c-cdc645b7b0f6',
+      device_id: process.env.SEAM_DEVICE_ID,
       name,
       starts_at,
       ends_at,
@@ -19,11 +19,58 @@ async function createAccessCode({
 
     return newCode;
   } catch (error) {
-    console.error('Error creating access code:', error.message);
-    throw new Error(`Access code creation failed: ${error.message}`);
+    throw new Error(`Access code could not be created: ${error.message}`);
+  }
+}
+
+async function getAccessCodeFromBookingId(bookingId) {
+  try {
+    const allCodes = await seam.accessCodes.list({
+      device_id: process.env.SEAM_DEVICE_ID,
+    });
+
+    const accessCode = allCodes.find((accessCode) => {
+      const match = accessCode.name.match(/Booking nr\.\s*(\d+)\s*for/);
+      return match && match[1] === String(bookingId);
+    });
+
+    return accessCode;
+  } catch (error) {
+    throw new Error(
+      `Access code for booking nr. ${bookingId} could not be found: ${error.message}`
+    );
+  }
+}
+
+async function updateAccessCode({ access_code_id, starts_at, ends_at }) {
+  try {
+    const updatedAccessCode = await seam.accessCodes.update({
+      access_code_id,
+      starts_at,
+      ends_at,
+    });
+
+    return updatedAccessCode;
+  } catch (error) {
+    throw new Error(`Access code could not be updated: ${error.message}`);
+  }
+}
+
+async function deleteAccessCode(id) {
+  try {
+    const deletedAccessCode = await seam.accessCodes.delete({
+      access_code_id: id,
+    });
+
+    return deletedAccessCode;
+  } catch (error) {
+    throw new Error(`Access code could not be deleted: ${error.message}`);
   }
 }
 
 module.exports = {
   createAccessCode,
+  getAccessCodeFromBookingId,
+  updateAccessCode,
+  deleteAccessCode,
 };
